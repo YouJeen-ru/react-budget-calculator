a2417fd8829ed8b79483d0b68b3bc31771228cd4
YouJeen-ru <1.rouch@mail.ru>
2020-10-12 18:41:40 +0300

    Updates

---
+++ precache-manifest.133ed86ecc1d8a77f2a0757680a61055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d0f84d1f09f5e719b1f266b8e664c1",
+    "url": "/react-budget-calculator/index.html"
+  },
+  {
+    "revision": "562f56855fde67810461",
+    "url": "/react-budget-calculator/static/css/main.508cb976.chunk.css"
+  },
+  {
+    "revision": "e1f10e31a745f938d94a",
+    "url": "/react-budget-calculator/static/js/2.c2977855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-calculator/static/js/2.c2977855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562f56855fde67810461",
+    "url": "/react-budget-calculator/static/js/main.d4dc6a92.chunk.js"
+  },
+  {
+    "revision": "2f28ab5205e188adedf9",
+    "url": "/react-budget-calculator/static/js/runtime-main.0459b74a.js"
+  }
+]);
